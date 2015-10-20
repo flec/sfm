a08f6361dd107b15dc16395b601351a01c883e85
nordewal <nordewal@gmail.com>
2015-10-20 19:51:06 +0200

    open dir using qt

--- src/ui/mainwindow/mainwindow.cpp
+++ src/ui/mainwindow/mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow) {
   ui->setupUi(this);
-  featureDetectionTab = this->findChild<FeatureDetectionTab*>("featureDetectionTab");
+  featureDetectionTab = this->findChild<FeatureDetectionTab *>("featureDetectionTab");
 }
 
 MainWindow::~MainWindow() {
@@ -17,15 +17,15 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::on_actionLoad_images_triggered() {
-  //QString images_dir = QFileDialog::getExistingDirectory(this, tr("Choose images directory"), "",
-  //                                                       QFileDialog::ShowDirsOnly);
-  sfmapp = new SFMApp("/tmp/temple/");
+  QString images_dir = QFileDialog::getExistingDirectory(this, tr("Choose images directory"), "",
+                                                         QFileDialog::ShowDirsOnly);
+  sfmapp = new SFMApp(images_dir.toStdString() + "/");
+  //sfmapp = new SFMApp("/tmp/temple/");
 
   featureDetectionTab->updateImages(&sfmapp->images);
 }
 
-void MainWindow::on_actionUnload_Images_triggered()
-{
+void MainWindow::on_actionUnload_Images_triggered() {
   sfmapp->unloadImages();
   featureDetectionTab->updateImages(&sfmapp->images);
 }
