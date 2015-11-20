32575e3263807c637bed83193feb21c83ee7b3ef
marco <mfuellemann@gmail.com>
2015-11-20 08:40:23 +0100

    added ply writer

---
+++ src/util/plyutil.cpp
@@ -0,0 +1,35 @@
+//
+// Created by marco on 19.11.15.
+//
+
+#include "plyutil.h"
+
+void PlyUtil::write(const string &fileName, const Mat &points3Dh) {
+
+  ofstream file(fileName.c_str());
+
+  if ( !file )
+  {
+    cerr << "Error opening output file: " << fileName << "!" << endl;
+    exit( 1 );
+  }
+
+  file << "ply" << endl;
+  file << "format ascii 1.0" << endl;
+  file << "element vertex " << points3Dh.rows << endl;
+  file << "property float x" << endl;
+  file << "property float y" << endl;
+  file << "property float z" << endl;
+  //file << "element face " << triangleNum << endl;
+  //file << "property list uchar int vertex_index" << endl;
+  file << "end_header" << endl;
+
+
+  for (int c = 0; c < points3Dh.cols; c++) {
+    for (int r = 0; r < 3; r++) {
+      file << points3Dh.at<double>(c, r) << " ";
+    }
+    file << endl;
+  }
+
+}
