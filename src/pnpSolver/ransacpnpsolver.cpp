dfb06ab7be2cd321709a482ee94decb3f98c6d80
marco <mfuellemann@gmail.com>
2015-11-22 18:19:55 +0100

    fixed pnpSolver

--- src/pnpSolver/ransacpnpsolver.cpp
+++ src/pnpSolver/ransacpnpsolver.cpp
@@ -12,8 +12,11 @@ void RANSACPnPSolver::solve(shared_ptr <ImagePair> &image_pair, Mat &intristic_c
   vector<KeyPoint> *keypoints2 = image_pair->image2->get_keypoints();
 
   for (auto match:image_pair->matches) {
-    objectPoints.push_back(*image_pair->image1->getObjectPoint(match.queryIdx)->getCoordinates());
-    imagePoints.push_back(keypoints2->at(match.trainIdx).pt);
+    shared_ptr<ObjectPoint> objectPoint = image_pair->image1->getObjectPoint(match.queryIdx);
+    if (objectPoint) {
+      objectPoints.push_back(*objectPoint->getCoordinates());
+      imagePoints.push_back(keypoints2->at(match.trainIdx).pt);
+    }
   }
 
   solvePnPRansac(objectPoints, imagePoints, intristic_camera_paramaters, noArray(), image_pair->rotation, image_pair->translation);
