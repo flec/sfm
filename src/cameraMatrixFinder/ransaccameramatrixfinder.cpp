a1750bef41a3c162120fa4333f00949803465bcb
nordewal <nordewal@gmail.com>
2015-12-11 10:44:53 +0100

    add comments to camera matrix finder

--- src/cameraMatrixFinder/ransaccameramatrixfinder.cpp
+++ src/cameraMatrixFinder/ransaccameramatrixfinder.cpp
@@ -11,7 +11,7 @@ void RANSACCameraMatrixFinder::findCameraMatrix(shared_ptr<ImagePair> &image_pai
   vector<Point2f> points_img1;
   vector<Point2f> points_img2;
 
-  // get keypoints_ from good matches
+  // get keypoints from matches as vector<Point2f>
   image_pair->getMatches(points_img1, points_img2);
 
   // Mask with inliners
@@ -31,33 +31,4 @@ void RANSACCameraMatrixFinder::findCameraMatrix(shared_ptr<ImagePair> &image_pai
               intristic_camera_paramaters.at<double>(0, 0), camera_center, inliners);
 
   removeOutlinerMatches(image_pair, inliners);
-
-
-//  Mat F = findFundamentalMat(points_img1, points_img2, CV_FM_8POINT, 2, 0.9999, inliners);
-//  Mat_<double> E = intristic_camera_paramaters.t() * F * intristic_camera_paramaters;
-
-//  // Extract transaltion and rotation
-//  SVD svd2(E,SVD::MODIFY_A);
-//  Mat svd_u2 = svd2.u;
-//  Mat svd_vt2 = svd2.vt;
-//  Mat svd_w2 = svd2.w;
-//  Matx33d W2(0,-1,0,//HZ 9.13
-//            1,0,0,
-//            0,0,1);
-//
-//
-//  Mat_<double> R= svd_u2 * Mat(W2).t() * svd_vt2;
-//  Mat_<double> T=svd_u2.col(2);
-//  cout<<intristic_camera_paramaters<<endl;
-//  cout<<F<<endl<<endl;
-//  // F should be around
-//  // -60.9979e-009     5.8124e-006     547.8079e-006
-//  // 11.0044e-006      709.6751e-009  -263.0490e-003
-//  // -1.7015e-003      253.8168e-003   933.1865e-003
-//  cout<<R<<endl<<endl;
-//  // R should be around
-//  // 996.5006e-003   -27.9893e-003    78.7595e-003
-//  // 27.8333e-003    999.6078e-003    3.0784e-003
-//  // -78.8148e-003   -875.4670e-006   996.8889e-003
-//  cout<<T<<endl;
 }
--- src/triangulator/triangulator.h
+++ src/triangulator/triangulator.h
@@ -2,16 +2,19 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#pragma once
+#ifndef SFM_TRIANGULATOR_H
+#define SFM_TRIANGULATOR_H
 
 #include <memory>
 #include "image/pair/imagepair.h"
 
 // Triangulates 2D points to obtain 3D points
-class Triangulator
-{
+class Triangulator {
 public:
   // computes homogenous 3D points for an image pair
-  virtual void findPoints3D(shared_ptr<ImagePair> &image_pair, Mat &intrinsic_camera_parameters, map<int, Point3f> &map_points3D) = 0;
+  virtual void findPoints3D(shared_ptr <ImagePair> &image_pair, Mat &intrinsic_camera_parameters,
+                            map<int, Point3f> &map_points3D) = 0;
 
 };
+
+#endif // SFM_TRIANGULATOR_H
