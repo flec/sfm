87485f837c26a7949b35a209059ab5fb4240fc95
marco <mfuellemann@gmail.com>
2015-12-11 16:15:33 +0100

    refactored util functions

--- src/util/fileutil.cpp
+++ src/util/fileutil.cpp
@@ -5,24 +5,18 @@
 #include "fileutil.h"
 
 string FileUtil::getPathName(const string &s) {
-
-
   size_t i = s.rfind(FILE_SEPARATOR, s.length());
   if (i != string::npos) {
     return (s.substr(0, i));
   }
-
   return ("");
 }
 
 
 string FileUtil::getFileName(const string &s) {
-
-
   size_t i = s.rfind(FILE_SEPARATOR, s.length());
   if (i != string::npos) {
     return (s.substr(i+1));
   }
-
   return ("");
 }
