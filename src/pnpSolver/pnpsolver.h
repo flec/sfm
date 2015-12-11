663e0849ce3a08287c093f35ebe48dccc55f091e
marco <mfuellemann@gmail.com>
2015-12-11 11:55:24 +0100

    refactored pnp solver

--- src/pnpSolver/pnpsolver.h
+++ src/pnpSolver/pnpsolver.h
@@ -7,8 +7,20 @@
 
 #include "image/pair/imagepair.h"
 
+/**
+ * Interface for a perspective-n-point pose estimation
+ * This is used to calculate the pose of a camera of a given image using the 3D object points and corresponding image points
+ */
 class PnPSolver {
 public:
+  /**
+   * Estimates the camera pose of the second image in the image pair.
+   * This is done by using the key point matches between the two images and corresponding 3D points of the first image.
+   * The estimated pose is directly updated in the second image
+   *
+   * image_pair                   pair of images where the pose is estimated in the second image
+   * intristic_camera_paramaters  intrinsic camera parameters
+   */
   virtual void solve(shared_ptr<ImagePair> &image_pair, Mat &intristic_camera_paramaters) = 0;
 };
 
--- src/pnpSolver/ransacpnpsolver.cpp
+++ src/pnpSolver/ransacpnpsolver.cpp
@@ -6,19 +6,24 @@
 #include "ransacpnpsolver.h"
 
 void RANSACPnPSolver::solve(shared_ptr<ImagePair> &image_pair, Mat &intristic_camera_paramaters) {
-  Mat rotEstimate = image_pair->image1->camera()->extrinsic_rotation() * image_pair->rotation;
-  Mat transEstimate = image_pair->image1->camera()->extrinsic_translation() + image_pair->translation;
+  // calculate estimate based on the first image
+  Mat rot_estimate = image_pair->image1->camera()->extrinsic_rotation() * image_pair->rotation;
+  Mat trans_estimate = image_pair->image1->camera()->extrinsic_translation() + image_pair->translation;
 
   Mat rvec;
   Mat tvec;
   Mat rot;
 
-  Rodrigues(rotEstimate, rvec);
-  tvec = transEstimate;
+  // convert estimate for use in solvePnPRansac
+  Rodrigues(rot_estimate, rvec);
+  tvec = trans_estimate;
 
+  // estimate pose of camera in second image
   solvePnPRansac(image_pair->pnp_object_points, image_pair->pnp_image_points, intristic_camera_paramaters,
                  noArray(), rvec, tvec, true, 100, 2.0, 0.99, noArray(), SOLVEPNP_EPNP);
 
+  // convert rotation to 3x3 matrix
   Rodrigues(rvec, rot);
+  // set estimated pose in camera of second image
   image_pair->image2->camera()->set_extrinsic(rot, tvec);
 }
