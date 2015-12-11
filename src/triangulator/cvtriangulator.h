12f07d38d26e77e64d4ceb56d2e53ba68cb34fbc
marco <mfuellemann@gmail.com>
2015-12-11 15:08:20 +0100

    refactored triangulator

--- src/triangulator/cvtriangulator.h
+++ src/triangulator/cvtriangulator.h
@@ -7,8 +7,16 @@
 
 #include "triangulator.h"
 
+/**
+ * Implementation of the triangulator using the OpenCV triangulation method
+ */
 class CVTriangulator : public Triangulator {
+private:
+  static const float MAX_REPROJECTION_ERROR;
 public:
+  /**
+   * @see Triangulator
+   */
   void findPoints3D(shared_ptr<ImagePair> &image_pair, Mat &intrinsic_camera_parameters, map<int, Point3f> &map_points3D);
 };
 #endif //SFM_BASETRIANGULATOR_H
--- src/triangulator/triangulator.h
+++ src/triangulator/triangulator.h
@@ -8,12 +8,20 @@
 #include <memory>
 #include "image/pair/imagepair.h"
 
-// Triangulates 2D points to obtain 3D points
+/**
+ * Interface for a triangulator that triangulates 2D points to obtain 3D points
+ */
 class Triangulator {
 public:
-  // computes homogenous 3D points for an image pair
-  virtual void findPoints3D(shared_ptr <ImagePair> &image_pair, Mat &intrinsic_camera_parameters,
-                            map<int, Point3f> &map_points3D) = 0;
+  /**
+   * Triangulates the specified points for triangulation in both images of the pair to obtain 3D points.
+   * If outliners are removed the map returned does only contain indices with inliners.
+   *
+   * image_pair                   pair of images that should be triangulated
+   * intrinsic_camera_parameters  intrinsic camera parameters
+   * map_points3D                 output: map with triangulated inliner 3D points where the key corresponds to the indices in vector of points for triangulation
+   */
+  virtual void findPoints3D(shared_ptr <ImagePair> &image_pair, Mat &intrinsic_camera_parameters, map<int, Point3f> &map_points3D) = 0;
 
 };
 
