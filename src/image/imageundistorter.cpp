c511ffb139d366a25bca1060246d32ea1db9298e
nordewal <nordewal@gmail.com>
2016-01-08 11:03:28 +0100

    fany cout for dist images

--- src/image/imageundistorter.cpp
+++ src/image/imageundistorter.cpp
@@ -3,11 +3,15 @@
 //
 
 #include <opencv2/imgproc.hpp>
+#include <iostream>
 #include "imageundistorter.h"
 
 void ImageUndistorter::undistort(vector<shared_ptr <Image>> images, Mat &intrinsic,
                                  Mat &distortion_coefficients) {
 
+#ifdef DEBUG
+  cout<<"Undistorting images with matrix: "<<distortion_coefficients<<endl;
+#endif
   Mat image_output;
   for (auto image : images) {
     // undistort color image
